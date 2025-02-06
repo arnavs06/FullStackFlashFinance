5f848980ab531659be4ed4376f6dc5bb00ba666a
arnavs06 <arnavsk1006@gmail.com>
2025-02-06 02:07:02 -0500

    cooool

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,7 +4,7 @@ import '../styles/index.css';
 const Hero = () => (
   <div className="hero">
     <h1>Master the Interview Process</h1>
-    <p>Your go-to resource for technical questions, valuation methods, and much more!</p>
+    <p>Your go-to resource for technical questions, valuation methods, and much more!</p><img src="/cool_thing_flashFinance.png" alt="Flash Finance Logo" />
   </div>
 );
 
